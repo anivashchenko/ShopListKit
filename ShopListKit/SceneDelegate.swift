487c329528d5c8317d5f72360f06c1df8665733f
Anastasiia <nastylik25@gmail.com>
2023-03-15 17:06:25 +0200

    Instantiate initial view controller from the `Main` storyboard as root view controller

--- ShopListKit/SceneDelegate.swift
+++ ShopListKit/SceneDelegate.swift
@@ -11,8 +11,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
+        let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = FoodListViewController()
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 }
