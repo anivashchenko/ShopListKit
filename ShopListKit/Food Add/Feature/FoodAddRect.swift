1c12e5adf7f5e3f16a0c1ed6bdd9764ad9f3a637
Anastasiia <nastylik25@gmail.com>
2023-03-22 16:49:26 +0200

    Add `addedToList` UILabel and UIView

--- ShopListKit/Food Add/Feature/FoodAddRect.swift
+++ ShopListKit/Food Add/Feature/FoodAddRect.swift
@@ -12,6 +12,18 @@ class FoodAddRect: UIView {
     @IBOutlet var stepper: UIView!
     @IBOutlet var addButton: UIButton!
 
+    private let addedToListLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let addedToListView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     private static let reuseIdentifier = String(describing: FoodAddRect.self)
 
     override init(frame: CGRect) {
@@ -25,7 +37,7 @@ class FoodAddRect: UIView {
     }
     
     private func commonInit() {
-        let _ = loadNibNamed()
+        let view = loadNibNamed()
 
         titleView.layer.cornerRadius = 10
         
@@ -38,6 +50,25 @@ class FoodAddRect: UIView {
                 color: UIColor.white.cgColor
             ), for: .normal
         )
+        
+        addedToListLabel.attributedText = customAttributedTitle("Succesfully added to the basket!", size: 18, color: .white)
+        addedToListLabel.textAlignment = .center
+        addedToListView.layer.cornerRadius = 10
+        addedToListView.backgroundColor = UIColor(.darkGreen)
+        view.addSubview(addedToListView)
+        view.addSubview(addedToListLabel)
+        
+        NSLayoutConstraint.activate([
+            addedToListView.leadingAnchor.constraint(equalTo: stepper.leadingAnchor),
+            addedToListView.trailingAnchor.constraint(equalTo: addButton.trailingAnchor),
+            addedToListView.bottomAnchor.constraint(equalTo: stepper.bottomAnchor),
+            addedToListView.heightAnchor.constraint(equalTo: stepper.heightAnchor),
+            
+            addedToListLabel.leadingAnchor.constraint(equalTo: addedToListView.leadingAnchor),
+            addedToListLabel.trailingAnchor.constraint(equalTo: addedToListView.trailingAnchor),
+            addedToListLabel.bottomAnchor.constraint(equalTo: addedToListView.bottomAnchor),
+            addedToListLabel.heightAnchor.constraint(equalTo: addedToListView.heightAnchor),
+        ])
         
         layer.cornerRadius = 15
         backgroundColor = UIColor(white: 0.5, alpha: 0.3)
