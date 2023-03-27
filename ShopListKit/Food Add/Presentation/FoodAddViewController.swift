f9fa435757adf1d25df78b4e2a4d50843fdf6732
Anastasiia <nastylik25@gmail.com>
2023-03-27 16:53:37 +0300

    Extract loading foodAddView to `loadFoodAddRectView` method

--- ShopListKit/Food Add/Presentation/FoodAddViewController.swift
+++ ShopListKit/Food Add/Presentation/FoodAddViewController.swift
@@ -16,27 +16,34 @@ class FoodAddViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = foodAddView as? FoodAddRect {
-            view.titleLabel.attributedText = customAttributedTitle(item.name, size: 30, color: .darkGreen)
-            view.imageView?.image = UIImage(named: item.name)
-            view.stepper.delegate = self
-            
-            if let item = foodModel.items.first(where: { $0.name == item.name }) {
-                let countValue = item.countValue
-                view.stepper.count.text = "\(countValue)"
-                changeBackgroundColor(view, count: countValue)
-            }
-            
-            view.onPressAddButton = { [weak self] count in
-                guard let self = self else { return }
-                
-                self.foodModel.addToBasket(item: self.item!, count: count)
-                self.navigationController?.popViewController(animated: true)
-            }
-        }
+        guard let customView = loadFoodAddRectView() else { return }
+        view.addSubview(customView)
         
         backgroundImage.image = UIImage(named: item.name)
         blurBackgroundImage(for: backgroundImage)
+    }
+    
+    private func loadFoodAddRectView() -> UIView? {
+        guard let view = foodAddView as? FoodAddRect else { return nil }
+        
+        view.titleLabel.attributedText = customAttributedTitle(item.name, size: 30, color: .darkGreen)
+        view.imageView?.image = UIImage(named: item.name)
+        view.stepper.delegate = self
+        
+        if let item = foodModel.items.first(where: { $0.name == item.name }) {
+            let countValue = item.countValue
+            view.stepper.count.text = "\(countValue)"
+            changeBackgroundColor(view, count: countValue)
+        }
+        
+        view.onPressAddButton = { [weak self] count in
+            guard let self = self else { return }
+            
+            self.foodModel.addToBasket(item: self.item!, count: count)
+            self.navigationController?.popViewController(animated: true)
+        }
+        
+        return view
     }
 }
 
