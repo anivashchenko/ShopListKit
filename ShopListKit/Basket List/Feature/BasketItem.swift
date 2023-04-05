d73947bd02eab50e1d7760b8ab1f9fb9c890b150
Anastasiia <nastylik25@gmail.com>
2023-04-05 13:09:47 +0300

    Improve design of `BasketItem`

--- ShopListKit/Basket List/Feature/BasketItem.swift
+++ ShopListKit/Basket List/Feature/BasketItem.swift
@@ -5,31 +5,43 @@
 import Foundation
 
 struct BasketItem: Identifiable {
+    typealias TypeFood = Item.TypeFood
+    
     var id = UUID().uuidString
     var name: String
     var countValue: Int
+    var typeFood: TypeFood
     var isAddedToList: Bool
     var isBought: Bool
     var isFavorite: Bool
-    var typeFood: Item.TypeFood
+    
+    init(name: String, countValue: Int, typeFood: TypeFood,
+         isAddedToList: Bool = true, isBought: Bool = false, isFavorite: Bool = false) {
+        self.name = name
+        self.countValue = countValue
+        self.isAddedToList = isAddedToList
+        self.isBought = isBought
+        self.isFavorite = isFavorite
+        self.typeFood = typeFood
+    }
     
     func updateCount(with oldCount: Int) -> BasketItem {
         BasketItem(
             name: name,
             countValue: countValue + oldCount,
+            typeFood: typeFood,
             isAddedToList: isAddedToList,
             isBought: isBought,
-            isFavorite: isFavorite,
-            typeFood: typeFood)
+            isFavorite: isFavorite)
     }
     
     func updateIsBought() -> BasketItem {
         BasketItem(
             name: name,
             countValue: countValue,
+            typeFood: typeFood,
             isAddedToList: !isAddedToList,
             isBought: !isBought,
-            isFavorite: isFavorite,
-            typeFood: typeFood)
+            isFavorite: isFavorite)
     }
 }
