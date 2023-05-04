fe21ee507e3db64bd76f290d446dffd930b0d976
Anastasiia <nastylik25@gmail.com>
2023-05-04 13:13:13 +0300

    Change mechanism of creating id from UUID to a computedProperty

--- ShopListKit/Basket List/Feature/BasketItem.swift
+++ ShopListKit/Basket List/Feature/BasketItem.swift
@@ -6,13 +6,16 @@ import Foundation
 
 struct BasketItem: Identifiable {
     
-    var id = UUID().uuidString
     var name: String
     var countValue: Int
     var typeFood: Item.TypeFood
     var isAddedToList: Bool = true
     var isBought: Bool = false
     var isFavorite: Bool = false
+    
+    var id: String {
+        get { name + "\(isAddedToList)" }
+    }
     
     mutating func updateCount(with oldCount: Int) {
         countValue += oldCount
