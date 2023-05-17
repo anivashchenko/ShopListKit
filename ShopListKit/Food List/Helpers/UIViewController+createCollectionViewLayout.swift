73b7324d4cf347ae62106c5445b7f36e29ffe95e
Anastasiia <nastylik25@gmail.com>
2023-05-17 12:16:56 +0300

    Improve `createCollectionViewLayout()`

--- ShopListKit/Food List/Helpers/UIViewController+createCollectionViewLayout.swift
+++ ShopListKit/Food List/Helpers/UIViewController+createCollectionViewLayout.swift
@@ -7,20 +7,21 @@ import UIKit
 extension UIViewController {
     
     func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout(sectionProvider: { sectionId, environment in
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
-                                                  heightDimension: .fractionalWidth(0.5))
+        UICollectionViewCompositionalLayout { sectionId, environment in
+            let halfOfWidth: NSCollectionLayoutDimension = .fractionalWidth(0.5)
+            let width: NSCollectionLayoutDimension = .fractionalWidth(1)
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: halfOfWidth, heightDimension: width)
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
             item.contentInsets = .init(top: 0, leading: 20, bottom: 20, trailing: 0)
             
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                   heightDimension: .fractionalWidth(0.5))
+            let groupSize = NSCollectionLayoutSize(widthDimension: width, heightDimension: halfOfWidth)
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
             
             let section = NSCollectionLayoutSection(group: group)
             section.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 20)
             
             return section
-        })
+        }
     }
 }
