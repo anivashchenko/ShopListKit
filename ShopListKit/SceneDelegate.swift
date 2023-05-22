6dffdae7bcf924ca87152461ab7fb48d188f2b48
Anastasiia <nastylik25@gmail.com>
2023-05-22 12:37:58 +0300

    Extract instantiation of view controllers to separate methods in `scene(:willConnectTo:options)`

--- ShopListKit/SceneDelegate.swift
+++ ShopListKit/SceneDelegate.swift
@@ -7,32 +7,43 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    var tabBarController: UITabBarController?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let tabBarController = storyboard.instantiateInitialViewController() as? UITabBarController
+        tabBarController = storyboard.instantiateInitialViewController() as? UITabBarController
         
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
         
         let basketModel = BasketModel()
-        let basketNavController = tabBarController?.viewControllers?[1] as? UINavigationController
-        let basketViewController = basketNavController?.viewControllers[0] as? BasketListViewController
-        basketViewController?.basketModel = basketModel
-        drawBasketIcon(in: basketNavController)
-        
-        let dataLoader = JsonDataLoader()
-        let foodModel = FoodListModel(items: dataLoader.loadData(), titles: dataLoader.titles, basketModel: basketModel)
-        
-        let navigationController = tabBarController?.viewControllers?[0] as? UINavigationController
-        let controller = navigationController?.viewControllers[0] as? FoodListViewController
-        controller?.foodModel = foodModel
+        instantiateBasketListViewController(basketModel: basketModel)
+        instantiateFoodListViewController(basketModel: basketModel)
     }
     
-    private func drawBasketIcon(in navigationController: UINavigationController?) {
+    private func instantiateBasketListViewController(basketModel: BasketModel) {
+        let basketlistVC = instantiateViewController(indexOfVCInTheTabBar: 1) as? BasketListViewController
+        basketlistVC?.basketModel = basketModel
+        drawBasketIcon()
+    }
+    
+    private func instantiateFoodListViewController(basketModel: BasketModel) {
+        let dataLoader = JsonDataLoader()
+        let foodModel = FoodListModel(items: dataLoader.loadData(), titles: dataLoader.titles, basketModel: basketModel)
+        let foodlistVC = instantiateViewController(indexOfVCInTheTabBar: 0) as? FoodListViewController
+        foodlistVC?.foodModel = foodModel
+    }
+    
+    private func instantiateViewController(indexOfVCInTheTabBar: Int) -> UIViewController? {
+        let navigationController = tabBarController?.viewControllers?[indexOfVCInTheTabBar] as? UINavigationController
+        return navigationController?.viewControllers.first
+    }
+    
+    private func drawBasketIcon() {
+        let navigationController = tabBarController?.viewControllers?.last as? UINavigationController
         let basketImage = UIImage.basketIconImage(height: 30, color: .gray)
         navigationController?.tabBarItem = UITabBarItem(title: "Basket", image: basketImage, tag: 2)
     }
